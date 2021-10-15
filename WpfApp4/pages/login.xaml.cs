0b714cec914d7aa7ff676ba72b105b7725fd62d5
faraon5573 <killerjeck02@gmail.com>
2021-10-15 11:44:36 +0300

    Добавьте файлы проекта.

---
+++ WpfApp4/pages/login.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для login.xaml
+    /// </summary>
+    public partial class login : Page
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                auth CurrentUser = BaseConnect.BaseModel.auth.FirstOrDefault(x => x.login == txtLogin.Text && x.password == txtPassword.Password);
+                if (CurrentUser != null)
+                {//сюда напишем алгоритм перехода на страницу в зависимости от роли
+                    switch (CurrentUser.role)
+                    {
+                        case 1:
+                            MessageBox.Show("Вы вошли как администратор!");
+                            LoadPages.MainFrame.Navigate(new adminMenu());
+                            break;
+                        case 2:
+                        default:
+                            MessageBox.Show("Вы вошли как пользователь!");
+                            LoadPages.MainFrame.Navigate(new info(CurrentUser));
+                        break;
+                            
+                    }
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Такого пользователя нет");
+                }
+            }
+           catch
+            {
+                MessageBox.Show("Произошла неизвестная ошибка!");
+            }         
+            
+        }
+
+        private void btnReg_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPages.MainFrame.Navigate(new reg());
+        }
+
+        
+    }
+}
