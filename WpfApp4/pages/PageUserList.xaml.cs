18823802661b69eff89ed5cb0dd4017ede456c06
Евгений Теснов <killerjeck02@gmail.com>
2021-11-12 10:48:19 +0300

    1

--- WpfApp4/pages/PageUserList.xaml.cs
+++ WpfApp4/pages/PageUserList.xaml.cs
@@ -23,6 +23,7 @@ namespace WpfApp4.pages
     {
         List<users> users;
         List<users> lu1;
+        PageChange pc = new PageChange();
         public PageUserList()
         {
             InitializeComponent();
@@ -33,11 +34,7 @@ namespace WpfApp4.pages
             lbGenderFilter.SelectedValuePath = "id";
             lbGenderFilter.DisplayMemberPath = "gender";
             lu1 = users;
-        }
-
-        private void lbUsersList_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
+            DataContext = pc;
         }
         private void lbTraits_Loaded(object sender, RoutedEventArgs e)
         {
@@ -50,12 +47,17 @@ namespace WpfApp4.pages
         private void btnReset_Click(object sender, RoutedEventArgs e)
         {
             lbUsersList.ItemsSource = users;
-            txtOT.Text = string.Empty;
-            txtDO.Text = string.Empty;
+            lu1 = users;
+            lbGenderFilter.SelectedIndex = -1;
+            txtNameFilter.Text = "";
+            txtOT.Text = "";
+            txtDO.Text = "";
+            txtPageCount.Text = "";
+            txtCurrentPage.Text = "";
         }
 
         private void Filter(object sender, RoutedEventArgs e)
-        {     
+        {
             try
             {
                 int OT = Convert.ToInt32(txtOT.Text) - 1;
@@ -64,105 +66,76 @@ namespace WpfApp4.pages
             }
             catch
             {
-                
+                //ничего не надо делать, если этот фильтр не применен
             }
             if (lbGenderFilter.SelectedValue != null)
             {
                 lu1 = lu1.Where(x => x.gender == (int)lbGenderFilter.SelectedValue).ToList();
+
             }
+
             if (txtNameFilter.Text != "")
             {
                 lu1 = lu1.Where(x => x.name.Contains(txtNameFilter.Text)).ToList();
+                lbUsersList.ItemsSource = users;
+
             }
 
             lbUsersList.ItemsSource = lu1;
+            pc.Countlist = lu1.Count;
         }
-        int currentpage = 1;
+
         private void GoPage_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            TextBlock tb = (TextBlock)sender;
-            int countlist = users.Count;
-            int countpage = Convert.ToInt32(txtPageCount.Text);
-            int countpages = countlist / countpage;
+            TextBlock tb = (TextBlock)sender;        
             switch (tb.Uid)
             {
                 case "prev":
-                    currentpage--;
-                    break;
-                case "1":
-                    if (currentpage < 3) currentpage = 1;
-                    else if (currentpage > countpages - 2) currentpage = countpages - 4;
-                    else currentpage -= 2;
-                    break;
-                case "2":
-                    if (currentpage < 3) currentpage = 2;
-                    else if (currentpage > countpages - 2) currentpage = countpages - 3;
-                    else currentpage--;
-                    break;
-                case "3":
-                    if (currentpage < 3) currentpage = 3;
-                    else if (currentpage > countpages - 2) currentpage = countpages - 2;
-                    break;
-                case "4":
-                    if (currentpage < 3) currentpage = 4;
-                    else if (currentpage > countpages - 2) currentpage = countpages - 1;
-                    else currentpage++;
-                    break;
-                case "5":
-                    if (currentpage < 3) currentpage = 5;
-                    else if (currentpage > countpages - 2) currentpage = countpages;
-                    else currentpage += 2;
+                    pc.CurrentPage--;
                     break;
                 case "next":
-                    currentpage++;
+                    pc.CurrentPage++;
                     break;
                 default:
-                    currentpage = 1;
+                    pc.CurrentPage = Convert.ToInt32(tb.Text);
                     break;
             }
-            if (currentpage < 1) currentpage = 1;
-            if (currentpage >= countpages) currentpage = countpages;
-
-            if (currentpage < 3)
-            {
-                txt1.Text = " 1 ";
-                txt2.Text = " 2 ";
-                txt3.Text = " 3 ";
-                txt4.Text = " 4 ";
-                txt5.Text = " 5 ";
-            }
-            else if (currentpage > countpages - 2)
-            {
-                txt1.Text = " " + (countpages - 4).ToString() + " ";
-                txt2.Text = " " + (countpages - 3).ToString() + " ";
-                txt3.Text = " " + (countpages - 2).ToString() + " ";
-                txt4.Text = " " + (countpages - 1).ToString() + " ";
-                txt5.Text = " " + (countpages).ToString() + " ";
-            }
-            else
-            {
-                txt1.Text = " " + (currentpage - 2).ToString() + " ";
-                txt2.Text = " " + (currentpage - 1).ToString() + " ";
-                txt3.Text = " " + (currentpage).ToString() + " ";
-                txt4.Text = " " + (currentpage + 1).ToString() + " ";
-                txt5.Text = " " + (currentpage + 2).ToString() + " ";
-            }
-            txtCurrentPage.Text = "Текущая страница: " + (currentpage).ToString();
-            lu1 = users.Skip(currentpage * countpage - countpage).Take(countpage).ToList();
-            lbUsersList.ItemsSource = lu1;
+            lbUsersList.ItemsSource = lu1.Skip(pc.CurrentPage * pc.CountPage - pc.CountPage).Take(pc.CountPage).ToList();
+            txtCurrentPage.Text = "Текущая страница: " + (pc.CurrentPage).ToString();
         }
         private void txtPageCount_TextChanged(object sender, TextChangedEventArgs e)
         {
             try
             {
-                lu1 = users.Take(Convert.ToInt32(txtPageCount.Text)).ToList();
-                lbUsersList.ItemsSource = lu1;
+                pc.CountPage = Convert.ToInt32(txtPageCount.Text);
             }
             catch
             {
-
+                pc.CountPage = lu1.Count;
+            }
+            pc.Countlist = users.Count;
+            lbUsersList.ItemsSource = lu1.Skip(0).Take(pc.CountPage).ToList();
+        }
+        private void UserImage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Image IMG = sender as Image;
+            int ind = Convert.ToInt32(IMG.Uid);
+            users U = BaseConnect.BaseModel.users.FirstOrDefault(x => x.id == ind);
+            BitmapImage BI;
+            switch (U.gender)
+            {
+                case 1:
+                    BI = new BitmapImage(new Uri(@"/images/male.jpg", UriKind.Relative));
+                    break;
+                case 2:
+                    BI = new BitmapImage(new Uri(@"/images/female.jpg", UriKind.Relative));
+                    break;
+                default:
+                    BI = new BitmapImage(new Uri(@"/images/other.jpg", UriKind.Relative));
+                    break;
             }
 
+            IMG.Source = BI;//помещаем картинку в image
         }
     }
 }
