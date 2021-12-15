26047fdf7afa9743faf8e6cf88ea4d2f98c3ddcb
Евгений Теснов <killerjeck02@gmail.com>
2021-12-15 16:51:23 +0300

    1

--- WpfApp4/pages/login.xaml.cs
+++ WpfApp4/pages/login.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp4.pages
 {
@@ -24,6 +26,8 @@ namespace WpfApp4.pages
         {
             InitializeComponent();
         }
+        string kode;
+        bool flagKode = false;
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
@@ -31,39 +35,85 @@ namespace WpfApp4.pages
             {
                 auth CurrentUser = BaseConnect.BaseModel.auth.FirstOrDefault(x => x.login == txtLogin.Text && x.password == txtPassword.Password);
                 if (CurrentUser != null)
-                {//сюда напишем алгоритм перехода на страницу в зависимости от роли
-                    switch (CurrentUser.role)
+                {
+                    if (flagKode == false)
                     {
-                        case 1:
-                            MessageBox.Show("Вы вошли как администратор!");
-                            LoadPages.MainFrame.Navigate(new PageUserList());
-                            break;
-                        case 2:
-                        default:
-                            MessageBox.Show("Вы вошли как пользователь!");
-                            LoadPages.MainFrame.Navigate(new info(CurrentUser));
-                        break;
-                            
+                        generateKey();
+                        flagKode = true;
                     }
-                    
+                    else if (kode == txtKey.Text)
+                    {
+                        switch (CurrentUser.role)
+                        {
+                            case 1:
+                                MessageBox.Show("Вы вошли как администратор");
+                                LoadPages.MainFrame.Navigate(new PageUserList());
+                                break;
+                            case 2:
+                            default:
+                                MessageBox.Show("Вы вошли как обычный пользователь");
+                                LoadPages.MainFrame.Navigate(new info(CurrentUser));
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Введенный код не подходит");
+                    }
                 }
                 else
                 {
                     MessageBox.Show("Такого пользователя нет");
                 }
             }
-           catch
+            catch
             {
-                MessageBox.Show("Произошла неизвестная ошибка!");
-            }         
-            
+                MessageBox.Show("Какая-то неизвестная ошибка");
+            }
+
         }
 
         private void btnReg_Click(object sender, RoutedEventArgs e)
         {
             LoadPages.MainFrame.Navigate(new reg());
         }
+        Random random = new Random();
+        private void generateKey()
+        {
+            imgRefresh.IsEnabled = false;
+            kode = "";
 
-        
+            for (int i = 0; i < 8; i++)
+            {
+                kode += ((char)random.Next(33, 122)).ToString();
+            }
+            txtKey.Text = kode;
+            MessageBox.Show(kode, "Введите код в соответствующее поле на форме.", MessageBoxButton.OK, MessageBoxImage.Information, MessageBoxResult.OK, MessageBoxOptions.RightAlign);
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Start();
+        }
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            imgRefresh.IsEnabled = true;
+            kode = ((char)random.Next(33, 122)).ToString();
+        }
+        private void Image_imgRefresh(object sender, MouseButtonEventArgs e)
+        {
+            txtKey.Text = " ";
+            generateKey();
+            flagKode = true;
+        }
+
+        private void txtKey_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnDiagr_Click(object sender, RoutedEventArgs e)
+        {
+            LoadPages.MainFrame.Navigate(new Diagrams());
+        }
     }
 }
